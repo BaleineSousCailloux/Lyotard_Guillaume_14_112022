41edeb2514382158258870afd957e438782a1a82
BaleineSousCailloux <djfreez29@gmail.com>
2022-11-22 17:47:30 +0100

    css createEmployee page + draft for store

--- hrnet/src/components/CreateEmployeeForm.jsx
+++ hrnet/src/components/CreateEmployeeForm.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import states from '../datas/states'
 import departments from '../datas/departments'
-// import { useStore } from 'react-redux'
 import DatePicker from 'react-date-picker/dist/entry.nostyle'
 import '../styles/DatePicker.css'
 import '../styles/Calendar.css'
 import Dropdown from 'react-dropdown'
 import '../styles/Dropdown.css'
-// import { loginUser } from '../stores/employeesStore'
+import { useDispatch } from 'react-redux'
+import { updateEmployeesList } from '../stores/employeesStore'
 
 function SignInForm() {
   const [firstName, setFirstName] = useState('')
@@ -22,111 +21,123 @@ function SignInForm() {
   const [zipCode, setZipCode] = useState('')
   const [department, setDepartment] = useState('')
 
-  // const store = useStore()
-  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  const submitForm = async () => {
-    // await loginUser(store, firstName, lastName)
-    navigate('/profile')
+  const submitForm = () => {
+    const newEmployee = {
+      firstName,
+      lastName,
+      birthDate,
+      startDate,
+      street,
+      stateName,
+      zipCode,
+      department,
+    }
+    dispatch(updateEmployeesList(newEmployee))
   }
 
   return (
     <div className="form">
-      <div className="form-identity">
-        <div className="input-wrapper">
-          <label htmlFor="firtName">First Name</label>
-          <input
-            type="text"
-            id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
+      <div className="form-private">
+        <div className="form-private-identity">
+          <div className="input-wrapper">
+            <label htmlFor="firtName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="birthDate">Date of Birth</label>
+            <DatePicker
+              type="date"
+              id="birthDate"
+              value={birthDate}
+              onChange={(e) => setBirthDate(e.target.value)}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="startDate">Start Date</label>
+            <DatePicker
+              type="date"
+              id="startDate"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </div>
         </div>
-        <div className="input-wrapper">
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            type="text"
-            id="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="birthDate">Date of Birth</label>
-          <DatePicker
-            type="date"
-            id="birthDate"
-            value={birthDate}
-            onChange={(e) => setBirthDate(e.target.value)}
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="startDate">Start Date</label>
-          <DatePicker
-            type="date"
-            id="startDate"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-          />
-        </div>
-      </div>
-      <div className="form-adress">
-        <div className="input-wrapper">
-          <label htmlFor="street">Street</label>
-          <input
-            type="text"
-            id="street"
-            value={street}
-            onChange={(e) => setStreet(e.target.value)}
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="city">City</label>
-          <input
-            type="text"
-            id="city"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="stateName">State</label>
-          <Dropdown
-            options={states.map((e) => ({
-              label: e.name,
-              value: e.abbreviation,
-            }))}
-            placeholder="Select a State"
-            type="text"
-            id="stateName"
-            value={stateName}
-            onChange={(e) => setStateName(e.target.value)}
-          />
-        </div>
-        <div className="input-wrapper">
-          <label htmlFor="zipCode">Zip Code</label>
-          <input
-            type="number"
-            id="zipCode"
-            value={zipCode}
-            onChange={(e) => setZipCode(e.target.value)}
-          />
+        <div className="form-private-adress">
+          <div className="input-wrapper">
+            <label htmlFor="street">Street</label>
+            <input
+              type="text"
+              id="street"
+              value={street}
+              onChange={(e) => setStreet(e.target.value)}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="city">City</label>
+            <input
+              type="text"
+              id="city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="stateName">State</label>
+            <Dropdown
+              options={states.map((e) => ({
+                label: e.name,
+                value: e.abbreviation,
+              }))}
+              placeholder="Select a State"
+              type="text"
+              id="stateName"
+              value={stateName}
+              onChange={(e) => setStateName(e.value)}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="zipCode">Zip Code</label>
+            <input
+              type="number"
+              id="zipCode"
+              value={zipCode}
+              onChange={(e) => setZipCode(e.target.value)}
+            />
+          </div>
         </div>
       </div>
       <div className="form-save">
-        <div className="input-wrapper">
-          <label htmlFor="department">Department</label>
+        <div className="input-wrapper dept-dropdown">
+          <label className="sr-only" htmlFor="department">
+            Department
+          </label>
           <Dropdown
             options={departments}
             placeholder="Select a Department"
             type="text"
             id="department"
             value={department}
-            onChange={(e) => setDepartment(e.target.value)}
+            onChange={(e) => setDepartment(e.value)}
           />
         </div>
-        <button title="save" action={submitForm}>
-          Save
+        <button className="validate" title="save" action={submitForm}>
+          SAVE
         </button>
       </div>
     </div>
--- hrnet/src/stores/employeesStore.js
+++ hrnet/src/stores/employeesStore.js
@@ -3,162 +3,70 @@ import { produce } from 'immer'
 
 // state
 const initialState = {
-  status: 'void',
-  token: null,
-  data: null,
+  employeesList: [
+    {
+      city: 'San Francisco',
+      birthDate: '10/02/1965',
+      department: 'Engineering',
+      firstName: 'Bruce',
+      lastName: 'Willis',
+      startDate: '10/02/1965',
+      state: 'CO',
+      street: 'Lombard',
+      zipCode: '102302',
+    },
+    {
+      city: 'Miami',
+      birthDate: '21/11/1972',
+      department: 'Human Resources',
+      firstName: 'Jason',
+      lastName: 'Statham',
+      startDate: '21/11/1972',
+      state: 'FL',
+      street: 'Ocean Side',
+      zipCode: '2310',
+    },
+    {
+      city: 'New York',
+      birthDate: '06/01/1968',
+      department: 'Engineering',
+      firstName: 'Tom',
+      lastName: 'Cruise',
+      startDate: '06/01/1968',
+      state: 'GA',
+      street: '5th Avenue',
+      zipCode: '53002',
+    },
+  ],
   error: null,
 }
 
-const LOGIN = 'user/login'
-const LOGIN_RESOLVED = 'user/loginResolved'
-const LOGIN_REJECTED = 'user/loginRejected'
-const LOGOUT = 'user/logout'
-const PROFILE = 'user/profile'
-const SET_PROFILE = 'user/setProfile'
-const PROFILE_RESOLVED = 'user/profileResolved'
-const PROFILE_REJECTED = 'user/profileRejected'
-const SET_PROFILE_RESOLVED = 'user/setProfileResolved'
-const SET_PROFILE_REJECTED = 'user/setProfileRejected'
-const userLogin = () => ({ type: LOGIN })
-const userLoginResolved = (data) => ({ type: LOGIN_RESOLVED, payload: data })
-const userLoginRejected = (error) => ({ type: LOGIN_REJECTED, payload: error })
-const userLogout = () => ({ type: LOGOUT })
-const userGetProfile = () => ({ type: PROFILE })
-const userSetProfile = () => ({ type: SET_PROFILE })
-const userProfileResolved = (data) => ({
-  type: PROFILE_RESOLVED,
-  payload: data,
+const CREATE_EMPLOYEE = 'employee/create'
+const createEmployee = () => ({
+  type: CREATE_EMPLOYEE,
 })
-const userProfileRejected = (error) => ({
-  type: PROFILE_REJECTED,
-  payload: error,
-})
-const userSetProfileResolved = (data) => ({
-  type: SET_PROFILE_RESOLVED,
-  payload: data,
-})
-const userSetProfileRejected = (error) => ({
-  type: SET_PROFILE_REJECTED,
-  payload: error,
-})
-
-const URL = 'http://localhost:3001/api/v1/user/'
+const ERROR = 'error'
+const errorDisplay = () => ({ type: ERROR })
 
 // actions
-// export async function loginUser(store, email, password) {
-//   const status = store.getState().status
-//   if (status === 'pending' || status === 'updating') {
-//     return
-//   }
-//   store.dispatch(userLogin())
-//   try {
-//     const response = await axios.post(URL + 'login', { email, password })
-//     const data = response.data.body.token
-//     store.dispatch(userLoginResolved(data))
-//   } catch (error) {
-//     store.dispatch(userLoginRejected(error))
-//   }
-// }
+export const updateEmployeesList = (newEmployee) => {
+  try {
+    store.dispatch(createEmployee(newEmployee))
+  } catch (error) {
+    store.dispatch(errorDisplay(error))
+  }
+}
 
-// export function logoutUser(store) {
-//   store.dispatch(userLogout())
-// }
-
-// //profile user : accès aux ressources protégées du backend, entête Authorization nécessaire avec un token
-// export const userProfile = async (token) => {
-//   const config = {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   }
-//   store.dispatch(userGetProfile())
-//   try {
-//     const response = await axios.post(URL + 'profile', {}, config)
-//     const data = response.data.body
-//     store.dispatch(userProfileResolved(data))
-//   } catch (error) {
-//     store.dispatch(userProfileRejected(error))
-//   }
-// }
-
-// //update user profile
-// export const updateUserData = async (newData, token) => {
-//   const config = {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   }
-//   store.dispatch(userSetProfile())
-//   try {
-//     const response = await axios.put(URL + 'profile', newData, config)
-//     const data = response.data.body
-//     store.dispatch(userSetProfileResolved(data))
-//   } catch (error) {
-//     store.dispatch(userSetProfileRejected(error))
-//   }
-// }
-
-// reducers (profile || login)
-function userReducer(state = initialState, action) {
+// reducers
+function employeeReducer(state = initialState, action) {
   return produce(state, (draft) => {
     switch (action.type) {
-      case PROFILE:
-      case SET_PROFILE:
-      case LOGIN: {
-        if (draft.status === 'void') {
-          draft.status = 'pending'
-          return
-        }
-        if (draft.status === 'rejected') {
-          draft.error = null
-          draft.status = 'pending'
-          return
-        }
-        if (draft.status === 'resolved') {
-          draft.status = 'updating'
-          return
-        }
+      case CREATE_EMPLOYEE: {
+        draft.employeesList = action.payload
         return
       }
-      case LOGIN_RESOLVED: {
-        if (draft.status === 'pending' || draft.status === 'updating') {
-          draft.token = action.payload
-          draft.status = 'resolved'
-          return
-        }
-        return
-      }
-      case LOGIN_REJECTED: {
-        if (draft.status === 'pending' || draft.status === 'updating') {
-          draft.error = action.payload
-          draft.token = null
-          draft.status = 'rejected'
-          return
-        }
-        return
-      }
-      case LOGOUT: {
-        draft.token = null
-        draft.status = 'resolved'
-        return
-      }
-      case SET_PROFILE_RESOLVED:
-      case PROFILE_RESOLVED: {
-        if (draft.status === 'pending' || draft.status === 'updating') {
-          draft.data = action.payload
-          draft.status = 'resolved'
-          return
-        }
-        return
-      }
-      case SET_PROFILE_REJECTED:
-      case PROFILE_REJECTED: {
-        if (draft.status === 'pending' || draft.status === 'updating') {
-          draft.error = action.payload
-          draft.data = null
-          draft.status = 'rejected'
-          return
-        }
+      case ERROR: {
+        draft.error = action.payload
         return
       }
       default:
@@ -170,7 +78,7 @@ function userReducer(state = initialState, action) {
 // on utilise combineReducer pour faire
 // fonctionner plusieurs reducers ensemble comme l'ajout des transactions
 const reducer = combineReducers({
-  user: userReducer,
+  user: employeeReducer,
 })
 
 // redux devtools
